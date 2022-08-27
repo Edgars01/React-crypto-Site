4d39098c729a550dad77f87054612d8b7b1c71e9
Edgars Svarups <miclex82@tutanota.com>
2022-08-28 00:48:18 +0300

    Api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-    'X-RapidAPI-Key': '98f1463c4bmsh05b403b73436978p12d58djsnb87f281a4b91',
+    'X-RapidAPI-Key': '',
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 }
 
